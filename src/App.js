fafafa94c79356dfb5c0da67d841cbf1899443e8
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-07-05 19:29:24 +0300

    Dev-Updated Controlled Form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./index.css";
+import { useState } from "react";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -24,21 +25,43 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
-    e.preventDefault()
-    
+    e.preventDefault();
+    if(!description) return
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem)
+
+    setDescription('')
+    setQuantity('')
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          setQuantity(e.target.value);
+          console.log(+e.target.value);
+        }}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="item..."></input>
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+          console.log(e.target.value);
+        }}
+      ></input>
       <button>Add</button>
     </form>
   );
